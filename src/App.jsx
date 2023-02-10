6a00fd51712a6663e405c6511905c4065faf3a5d
Nebojsa Pejnovic <nebojsa.pejnovic@gmail.com>
2023-02-10 13:11:07 +0100

    all

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,48 @@ const VirtualWallet = () => {
   const [balance, setBalance] = useState(0);
   const [transactions, setTransactions] = useState([]);
 
-  // Load balance and transactions from local storage
+  // Load balance and transactions from IndexedDB
   React.useEffect(() => {
-    const storedBalance = localStorage.getItem("balance");
-    const storedTransactions = JSON.parse(localStorage.getItem("transactions"));
-
-    if (storedBalance) {
-      setBalance(Number(storedBalance));
-    }
-
-    if (storedTransactions) {
-      setTransactions(storedTransactions);
-    }
+    const openDB = async () => {
+      const db = await window.indexedDB.open("wallet", 1);
+      db.onupgradeneeded = (event) => {
+        const objectStore = event.target.result.createObjectStore("data", {
+          keyPath: "id",
+        });
+        objectStore.createIndex("balance", "balance", { unique: false });
+        objectStore.createIndex("transactions", "transactions", {
+          unique: false,
+        });
+      };
+      db.onsuccess = (event) => {
+        const objectStore = event.target.result
+          .transaction("data", "readonly")
+          .objectStore("data");
+        const balanceRequest = objectStore.index("balance").get(1);
+        balanceRequest.onsuccess = (event) => {
+          setBalance(event.target.result.balance || 0);
+        };
+        const transactionsRequest = objectStore.index("transactions").get(1);
+        transactionsRequest.onsuccess = (event) => {
+          setTransactions(event.target.result.transactions || []);
+        };
+      };
+    };
+    openDB();
   }, []);
 
-  // Save balance and transactions to local storage
+  // Save balance and transactions to IndexedDB
   React.useEffect(() => {
-    localStorage.setItem("balance", balance);
-    localStorage.setItem("transactions", JSON.stringify(transactions));
+    const saveData = async () => {
+      const db = await window.indexedDB.open("wallet", 1);
+      db.onsuccess = (event) => {
+        const objectStore = event.target.result
+          .transaction("data", "readwrite")
+          .objectStore("data");
+        objectStore.put({ id: 1, balance, transactions });
+      };
+    };
+    saveData();
   }, [balance, transactions]);
 
   const deposit = (amount) => {
@@ -38,20 +62,20 @@ const VirtualWallet = () => {
 
   return (
     <div>
-      <h1>Virtual Wallet</h1>
-      <p>Balance: {balance}</p>
-      <button onClick={() => deposit(10)}>Deposit $10</button>
-      <button onClick={() => withdraw(10)}>Withdraw $10</button>
-      <h2>Transaction History</h2>
-      <ul>
-        {transactions.map((transaction, index) => (
-          <li key={index}>
-            {transaction.type}: {transaction.amount}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+        <h1>Virtual Wallet</h1>
+        <p>Balance: {balance}</p>
+        <button onClick={() => deposit(10)}>Deposit $10</button>
+        <button onClick={() => withdraw(10)}>Withdraw $10</button>
+        <h2>Transaction History</h2>
+        <ul>
+          {transactions.map((transaction, index) => (
+            <li key={index}>
+              {transaction.type}: {transaction.amount}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
-export default VirtualWallet;
+  export default VirtualWallet;
